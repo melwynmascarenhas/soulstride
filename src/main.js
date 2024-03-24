9a0f825b797cd67a23403265a043f3358028678f
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2024-03-24 19:57:04 +0530

    horizontal scroll

--- src/main.js
+++ src/main.js
@@ -1,8 +1,54 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+/* eslint-disable */
+window.alert('local')
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
-console.log('Welcome to Vite + JS + Webflow!')
+gsap.registerPlugin(ScrollTrigger)
 
-createBadge()
-animateTitle()
+const horizontalSection = document.querySelector('.layout')
+const wrapper = horizontalSection.querySelector('.wrapper')
+const items = wrapper.querySelectorAll('.item')
+
+//function for the horizontal scrolling effect and parallax effect
+
+function initScroll() {
+  //horizontal scrolling effect
+  let wrapperTween = gsap.to(wrapper, {
+    x: () => (wrapper.offsetWidth - window.innerWidth) * -1,
+    ease: 'none',
+    scrollTrigger: {
+      markers: true,
+      trigger: horizontalSection,
+      pin: true,
+      start: 'top top',
+      end: () => `+=${items[0].offsetWidth * items.length}`,
+      scrub: 1,
+      invalidateOnRefresh: true, //recalculate the start and end points when window resize
+    },
+  })
+
+  //parallax effect
+  items.forEach((item) => {
+    gsap.fromTo(
+      item,
+      {
+        backgroundPosition: '0% 50%',
+      },
+      {
+        backgroundPosition: '100% 50%',
+        ease: 'none',
+        scrollTrigger: {
+          trigger: item,
+          containerAnimation: wrapperTween, //link the animation to the main scrolltrigger
+          //sets this trigger as it is nested in another scrolltrigger
+          start: 'left right',
+          end: 'right left',
+          scrub: true,
+          markers: true,
+        },
+      }
+    )
+  })
+}
+
+initScroll()
