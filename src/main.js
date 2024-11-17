0f5e05eb67d90ec4c1ccf572281dbf7267353e82
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2024-11-17 18:10:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -40,9 +40,6 @@ function enableScrolling() {
   //LENIS
   const lenis = new Lenis()
 
-  lenis.on('scroll', (e) => {
-    console.log(e)
-  })
   lenis.on('scroll', ScrollTrigger.update)
   gsap.ticker.add((time) => {
     lenis.raf(time * 1000)
